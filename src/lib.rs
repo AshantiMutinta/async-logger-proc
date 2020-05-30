4285747269b37d6f3561c9e540b7c5b2acf02a83
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2020-05-29 22:48:34 -0400

    removed some clones

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,33 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-use syn::ItemFn;
 use quote::quote;
-use quote::{ToTokens};
-
+use quote::ToTokens;
+use syn::ItemFn;
 
 struct AsyncLogger;
-
-
 
 /// Example of user-defined [derive mode macro][1]
 ///
 /// [1]: https://doc.rust-lang.org/reference/procedural-macros.html#derive-mode-macros
 #[proc_macro_attribute]
-pub fn async_logger(_attr: TokenStream,item: TokenStream) -> TokenStream {
+pub fn async_logger(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemFn);
-    let mut fold : Box<dyn syn::fold::Fold>  = Box::new(AsyncLogger);
+    let mut fold: Box<dyn syn::fold::Fold> = Box::new(AsyncLogger);
     let tokens = fold.fold_item_fn(input);
     tokens.to_token_stream().into()
 }
 
-impl syn::fold::Fold for AsyncLogger{
-
-    fn fold_item_fn(&mut self, function: ItemFn)-> syn::ItemFn
-    {
-        let mut function = function.clone();
+impl syn::fold::Fold for AsyncLogger {
+    fn fold_item_fn(&mut self, function: ItemFn) -> syn::ItemFn {
+        let mut function = function;
         let function_name = function.sig.ident.to_string();
         let block = function.block;
-        let new_block = quote!{ {
+        let new_block = quote! { {
             span!(#function_name,#block) }
         };
-        function.block = syn::parse(TokenStream::from(new_block.clone())).expect("Could not rewrite block with procoedural logger macro");
+        function.block = syn::parse(TokenStream::from(new_block))
+            .expect("Could not rewrite block with procoedural logger macro");
         function
-
     }
 }
-
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,14 +2,11 @@ use async_log::span;
 use async_logger_proc_macro::async_logger;
 
 #[async_logger]
-fn check_this_out()
-{
+fn check_this_out() {
     println!("test_this")
 }
 
-
 #[test]
-fn test_function()
-{
+fn test_function() {
     check_this_out();
 }
